a454565ce0f1a1ba488a228713682603b873cb91
황중섭 <jshwang9709@gmail.com>
2023-06-04 21:59:21 +0900

    회원가입 컨트롤러 생성

--- src/main/java/com/theater/domain/member/Member.java
+++ src/main/java/com/theater/domain/member/Member.java
@@ -2,10 +2,11 @@ package com.theater.domain.member;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //회원정보
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
 
     private String id; //아이디
@@ -13,6 +14,13 @@ public class Member {
     private String name; //이름
     private String phone; //전화번호
     private String email; //이메일주소
-    private Integer money; //보유금액
+    private Integer money = 0; //보유금액
 
+    public Member(String id, String password, String name, String phone, String email) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
 }
