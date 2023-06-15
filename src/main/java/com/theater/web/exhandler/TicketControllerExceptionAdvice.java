66110a1c78b975a26c4fdc0b904d0fa940163e2a
황중섭 <jshwang9709@gmail.com>
2023-06-15 12:27:29 +0900

    티켓 구매 및 처리 예외 케이스 추가

--- src/main/java/com/theater/web/exhandler/TicketControllerExceptionAdvice.java
+++ src/main/java/com/theater/web/exhandler/TicketControllerExceptionAdvice.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.management.AttributeNotFoundException;
 import javax.security.sasl.AuthenticationException;
 
 @RestControllerAdvice(assignableTypes = {TicketController.class}) //TicketController에서 예외가 발생하면 모두 이곳으로 옴
@@ -28,14 +29,14 @@ public class TicketControllerExceptionAdvice {
             message = e.getMessage();
             status = HttpStatus.BAD_REQUEST;
             errorCode = -500;
-        } else if (e instanceof MissingServletRequestParameterException) {
-            message = "쿼리 파라미터 all은 필수값입니다.";
+        } else if (e instanceof AttributeNotFoundException) {
+            message = e.getMessage();
             status = HttpStatus.BAD_REQUEST;
-            errorCode = -511;
+            errorCode = -501;
         } else if (e instanceof IllegalArgumentException) {
             message = e.getMessage();
             status = HttpStatus.BAD_REQUEST;
-            errorCode = -512;
+            errorCode = -513;
         } else { //위의 조건문에서 처리하지 못한, 알 수 없는 에러가 발생한 경우 Http 상태코드=500, errorCode=-599 로 반환
             message = e.getMessage();
             status = HttpStatus.INTERNAL_SERVER_ERROR;
